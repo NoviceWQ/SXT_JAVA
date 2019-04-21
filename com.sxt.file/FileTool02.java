98c2b338254e705b99567934e56edb43441018a0
NoviceWQ <45143577+NoviceWQ@users.noreply.github.com>
2019-04-20 22:49:49 -0500

    文件夹的遍历

---
+++ com.sxt.file/FileTool02.java
@@ -0,0 +1,27 @@
+package File类;
+
+import java.io.File;
+public class DiGuiDemo2 {
+
+	public static void main(String[] args) {
+		// 创建File对象
+		File dir = new File("F:\\N4X\\123");
+		printDir(dir);
+	}
+	
+	public static void printDir(File dir) {
+		// 获取子文件和目录
+		File[] files = dir.listFiles();
+		for(File file:files) {
+			//当是文件时,打印绝对路径.
+			if(file.isFile()) {
+				System.out.println("文件名:  "+file.getAbsolutePath());
+			//当是目录时,继续调用打印目录的方法,形成递归调用.
+			}else {
+				System.out.println("目录名:  "+file.getAbsolutePath());
+				printDir(file);
+			}
+		}
+	}
+
+}
